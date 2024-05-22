2cf9570522ac01543bd46656c04448b3121a47da
BlueSkiesPirate <sebastianreyna128@gmail.com>
2024-05-22 17:34:06 -0400

    DailyTasks Static

--- src/assets/Pages/DailyTasksPage.jsx
+++ src/assets/Pages/DailyTasksPage.jsx
@@ -7,20 +7,30 @@ export default function DailyTasks(){
         <div id={styles.TopBackground}>
             <div id={styles.TopContainer}>
                 <div id={styles.TopNav}>
-
+                    <div id={styles.NavLeft}>
+                    <img id={styles.menu} src="https://cdn.icon-icons.com/icons2/1875/PNG/512/hamburgermenu_120234.png"/>
+                    </div>
+                    <div id={styles.NavRight}>
+                        <div id={styles.PreviousBtn} className={` ${styles.TextFont}`}>
+                            Previous
+                        </div>
+                        <div id={styles.NextBtn} className={` ${styles.TextFont}`}>
+                            Next
+                        </div>
+                    </div>
                 </div>
                 <div id={styles.MainDash}>
 
-                    <div id={styles.DashLeft}>
+                    <div id={styles.DashLeft} className={styles.TextFont}>
                         Daily
                     </div>
                     <div id={styles.DashRight}>
-                        <div id={styles.DashDate}>
+                        <div id={styles.DashDate} className={styles.TextFont}>
                                 Mon 3/17/2024
                         </div>
                         <div id={styles.CreateContainer}>
-                            <div id={styles.CreateButton}>
-                                Create      +
+                            <div id={styles.CreateButton} className={styles.TextFont}>
+                                CREATE     +
                             </div>
                         </div>
 
@@ -30,6 +40,47 @@ export default function DailyTasks(){
 
         </div>
         <div id={styles.BottomBackground}>
+            <div id={styles.TaskContainer}>
+                <div id={styles.TaskContainerLeft}>
+                    <div id={styles.TaskTitle} className={styles.TextFont}>
+                        Wash Dishes
+                    </div>
+                    <div id={styles.TaskRewards}>
+                        <div id={styles.MaxRewardTitle} className={styles.TextFont}>
+                                Max Reward:
+                        </div>
+                        <div id={styles.MaxReward}>
+                            <div className={`${styles.Reward} ${styles.TextFont}`} >
+                               200 XP
+                            </div>
+                            <div className={`${styles.Reward} ${styles.TextFont}`}>
+                                $ 150
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div id={styles.TaskContainerRight}>
+                    <div id={styles.TaskDetails}>
+                        <div id={styles.Duration}>
+                            <div className={`${styles.Reward} ${styles.TextFont}`}>
+                               Duration
+                            </div>
+                            <div className={`${styles.Reward} ${styles.TextFont}`}>
+                                20 min
+                            </div>
+                        </div>
+                        <div id={styles.Difficultycontainer} className={` ${styles.TextFont}`}>
+                              <div className={`${styles.Difficulty}`}>
+                            </div> 
+                             Difficulty
+                        </div>
+                    </div>
+                    <div id={styles.TaskActivate}>
+                        <button id={styles.ActivateBtn} className={` ${styles.TextFont}`}>Activate</button>
+                    </div>
+                </div>
+            </div>
 
         </div>
         </>
