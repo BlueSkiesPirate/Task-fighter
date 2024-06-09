df4c37da2e0e1c3a506f20ceff22fa41ed138efb
BlueSkiesPirate <sebastianreyna128@gmail.com>
2024-06-09 00:30:48 -0400

    some minor changes

--- src/assets/Pages/AchievementsPage.jsx
+++ src/assets/Pages/AchievementsPage.jsx
@@ -2,39 +2,23 @@ import styles from "./AchievementsPage.module.css"
 export default function achievements(){
     return(
         <>
-    <div id={styles.topNav} >=</div>
-
-    <div id={styles.userContainer}>
-
-  <img id={styles.UserImage} src="task-fighter\public\vite.svg"/>
-  Task_Fighter
-    </div>
-
-    <div id={styles.detailsContainer}>
-        about
-        <div id={styles.aboutInfoContainer}><div id={styles.aboutInfo}/></div>
-        statistics
-        <div id={styles.statsContainer}>
-        <div id={styles.stats}>
-            <hr/>
-            day streak
-            <hr/>
-            HT
-            <hr/>
-            Total tasks
-            <hr/>
-            Rank
+        <div id={styles.topNav}>
+            <img id={styles.achievementsIcon} href=""></img>
+            Achievements
         </div>
-        </div>
+        <div id={styles.AchievementsContainer}>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+            <div className={styles.achievement}/>
+
         
-
-        <div id={styles.btnContainer}>
-            <button id={styles.AchievementsBtn} className={styles.btn}>Achievements</button>
-            <button id={styles.FeedbackBtn} className={styles.btn}>Feedback</button>
+            
         </div>
-
-    </div>
-
         </>
     )
 }
