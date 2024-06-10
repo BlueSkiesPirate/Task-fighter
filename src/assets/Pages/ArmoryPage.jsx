615ca27fa01c9736660384e14eccbb5220c4518b
BlueSkiesPirate <sebastianreyna128@gmail.com>
2024-06-09 21:33:12 -0400

    projectsPage, ArmoryPage, AchievementsPage created

--- src/assets/Pages/ArmoryPage.jsx
+++ src/assets/Pages/ArmoryPage.jsx
@@ -1,6 +1,50 @@
+import styles from "./ArmoryPage.module.css"
+
 export default function armory(){
     return(
         <>
+        <div id={styles.Background}>
+            <div id={styles.NavBar}>
+                MENU
+            </div>
+        <div id={styles.MenuBottom}>MENU_low</div>
+
+
+        <div id={styles.userEquipmentContainer}>
+            <div className={styles.userEquipment}>
+                
+     
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+
+
+            </div>
+            <div>2</div>
+
+            <div className={styles.userEquipment}>
+                
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+          
+            </div>
+        </div>
+        </div>
+
+        <div id={styles.ItemsContainer}>
+        <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+            <div className={styles.ItContOut}><div className={styles.ItContIn}></div></div>
+        </div>
+
+
         </>
     )
 }
--- src/assets/Pages/ProjectsPage.jsx
+++ src/assets/Pages/ProjectsPage.jsx
@@ -1,6 +1,35 @@
+import styles from "./ProjectsPage.module.css"
+
 export default function Projects(){
     return(
         <>
+        <div id={styles.mainContainer}>
+            <div id={styles.topNav}>
+                Projects
+            </div>
+
+            <div id={styles.projectsContainer}>
+
+                <div id={styles.project}>
+                    <div id={styles.projectLeft}>
+                        <div>Wireframe</div>
+                        <div id={styles.viewBtnContainer}>
+                            <button id={styles.viewBtn}>View</button>
+                        </div>
+                    </div>
+                    <div id={styles.projectRight}>
+                        <div>share</div>
+                        <div id={styles.percentageContainer}>
+                            <div id={styles.daily_tasks_middle}>
+                                <div id={styles.outerCircle}>
+                                    <div id={styles.innerCircle}> 60% </div>
+                                </div>
+                            </div> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     )
 }
