36245f2dc170281d436c6e969cb3c06e3e52e6a4
BlueSkiesPirate <sebastianreyna128@gmail.com>
2024-06-11 22:25:43 -0400

    began to create the menu button

--- src/assets/Pages/ShopPage.jsx
+++ src/assets/Pages/ShopPage.jsx
@@ -1,8 +1,22 @@
+import styles from "./ShopPage.module.css"
+
 export default function Shop(){
     return(
         <>
         
-        
+        <div id={styles.pageheader}>
+            Shop
+        </div>
+        <div id={styles.navBarContainer}>
+            Avatar customize
+        </div>
+        <div id={styles.availableShopItemsContainer}>
+            <div className={styles.item}>0</div>
+            <div className={styles.item}>0</div>
+            <div className={styles.item}>0</div>
+            <div className={styles.item}>0</div> 
+            <div className={styles.item}>0</div>
+        </div>
         </>
     )
 }
--- src/assets/Pages/SideQuestsPage.jsx
+++ src/assets/Pages/SideQuestsPage.jsx
@@ -1,7 +1,17 @@
+import styles from "./sideQuestsPage.module.css"
+
 export default function SideQuests(){
     return(
         <>
-        
+        <div id={styles.header}>
+             
+        Side Quests
+        </div>
+        <div id={styles.sideQuestContainer}>
+            <div id={styles.sideQuest}>
+                Check Email
+            </div>
+        </div>
         
         </>
     )
